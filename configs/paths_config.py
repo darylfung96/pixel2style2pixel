a0f11bd3f9746b29554f0546ee6158d64001f6b8
Daryl Fung <darylfung96@gmail.com>
2020-11-13 23:56:33 -0600

    added path

--- configs/paths_config.py
+++ configs/paths_config.py
@@ -1,11 +1,11 @@
 dataset_paths = {
 	'celeba_train': '',
-	'celeba_test': '',
+	'celeba_test': '/content/drive/My Drive/gan_daryl',
 	'celeba_train_sketch': '',
 	'celeba_test_sketch': '',
 	'celeba_train_segmentation': '',
 	'celeba_test_segmentation': '',
-	'ffhq': '',
+	'ffhq': '/content/drive/My Drive/gan_daryl',
 }
 
 model_paths = {
